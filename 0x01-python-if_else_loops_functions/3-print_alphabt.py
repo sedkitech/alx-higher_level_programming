7acf7ab73000d881aa793c7b336d3e1cd21a3e9a
sedkitech <dhiasedkipro@gmail.com>
2023-06-07 15:12:13 -0700

    correcting output

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for alphabetL in range(97, 123):
-    if (chr(alphabetL) != 'q' and chr(alphabetL) != 'c'):
+    if (chr(alphabetL) != 'q' and chr(alphabetL) != 'e'):
         print('{}'.format(chr(alphabetL)), end='')

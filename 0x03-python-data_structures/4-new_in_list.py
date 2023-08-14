458cd0527e6e3aae4ed2a1d5069ec32e0dddd0ed
sedkitech <dhiasedkipro@gmail.com>
2023-08-14 07:39:22 -0700

    refactoring conditions

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,8 @@
 def new_in_list(my_list, idx, element):
     new_list = my_list[:]
-    if (idx < 0) or (idx >= len(my_list)):
+    if (idx < 0):
+        return new_list
+    if (idx >= len(my_list)):
         return new_list
     new_list[idx] = element
     return new_list

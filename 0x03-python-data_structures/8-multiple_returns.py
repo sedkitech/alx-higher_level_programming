6b73707755cc1aff665c52c325f079c1f2c7a391
sedkitech <dhiasedkipro@gmail.com>
2023-08-15 10:39:02 -0700

    removing tup and returning multp

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tup = ("x", "y")
     if len(sentence) == 0:
-        return tup(0, None)
-    tup = (len(sentence), sentence[0])
-    return tup
+        return 0, None
+    return len(sentence), sentence[0]

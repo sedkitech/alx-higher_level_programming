06ee35cad3afda7184afec0db9494fc22194620a
sedkitech <dhiasedkipro@gmail.com>
2023-08-30 12:15:36 -0700

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""module"""
+
+
+class Square:
+    """class Square"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    """init class"""
+    def area(self):
+        return self.__size * self.__size
